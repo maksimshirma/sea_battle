608368804e141bf680b1b07c2b106c58e8609dbb
Maksim Shirma <maksimshirma@mail.ru>
2023-10-07 17:01:51 +0300

    added redux persistor

--- src/app/providers/with-store.tsx
+++ src/app/providers/with-store.tsx
@@ -1,8 +1,15 @@
 import { Provider } from "react-redux";
-import { store } from "../store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "../store/store";
 
 const StoreWrapper = ({ children }: { children: React.ReactNode }) => {
-    return <Provider store={store}>{children}</Provider>;
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                {children}
+            </PersistGate>
+        </Provider>
+    );
 };
 
 export default StoreWrapper;
--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,14 +1,25 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from "./userSlice/userSlice";
 
 const rootReducer = combineReducers({
     user: userReducer,
 });
 
+const persistConfig = {
+    key: "root",
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
